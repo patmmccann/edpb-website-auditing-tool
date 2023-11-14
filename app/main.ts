a26350e6794d4ba2c036162c9709acfa1560ca5f
jgn <gorinje@gmail.com>
2023-11-14 11:26:06 +0100

    Add consistency to names

--- app/main.ts
+++ app/main.ts
@@ -6,9 +6,9 @@ import * as url from 'url';
 
 const gotTheLock = app.requestSingleInstanceLock();
 
-import {BrowsersHandlher} from "./handlers/browser";
-import { ReportsHandlher } from './handlers/report';
-import { ParserHandlher } from './handlers/parser';
+import {BrowsersHandlher} from "./handlers/browser-handler";
+import { ReportsHandlher } from './handlers/report-handler';
+import { ParserHandlher } from './handlers/parser-hanlder';
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
