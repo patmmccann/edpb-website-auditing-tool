68ad404e3278f46606e5276323833db548432623
Jérôme Gorin <gorinje@gmail.com>
2024-01-05 10:37:00 +0100

    Fix all tests

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -50,6 +50,7 @@ import {MatTreeModule} from '@angular/material/tree';
 import {OverlayModule} from '@angular/cdk/overlay';
 import {CdkMenuModule} from '@angular/cdk/menu';
 import {DialogModule} from '@angular/cdk/dialog';
+import {MatBottomSheetRef, MAT_BOTTOM_SHEET_DATA} from '@angular/material/bottom-sheet';
 
 @NgModule({
   exports: [
@@ -99,7 +100,11 @@ import {DialogModule} from '@angular/cdk/dialog';
     OverlayModule,
     PortalModule,
     ScrollingModule,
-    DialogModule,
-  ]
+    DialogModule
+  ],
+  providers: [
+    { provide: MatBottomSheetRef, useValue: {} },
+    { provide: MAT_BOTTOM_SHEET_DATA, useValue: {} }
+  ],
 })
 export class MaterialAllModule {}
