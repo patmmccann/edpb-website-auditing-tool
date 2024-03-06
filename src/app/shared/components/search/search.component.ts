d023bc18f9ce0989bd5eefae6a1a44114b753b8c
Jérôme Gorin <gorinje@gmail.com>
2024-03-06 12:38:25 +0100

    Improve evaluation search

--- src/app/shared/components/search/search.component.ts
+++ src/app/shared/components/search/search.component.ts
@@ -1,10 +1,10 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
 import { Card } from 'src/app/models/card.model';
 import { BeaconLine } from 'src/app/models/cards/beacon-card.model';
 import { CookieLine } from 'src/app/models/cards/cookie-card.model';
 import { LocalStorageLine } from 'src/app/models/cards/local-storage-card.model';
-import { Status } from 'src/app/models/evaluation.model';
+import { Status, allStatus } from 'src/app/models/evaluation.model';
 import { KnowledgeBase } from 'src/app/models/knowledgeBase.model';
 import { FilterForStatus, filterForBeacon, filterForCookie, filterForLocalStorage } from 'src/app/pipes/filters.pipe';
 import { CardService } from 'src/app/services/card.service';
@@ -24,7 +24,6 @@ export class SearchComponent {
 
   @Output() searchStatusEvent = new EventEmitter<any>();
   searchStatusForm :FormGroup = new FormGroup({});
-  searchStatus:Status[] = [];
 
   @Output() searchCookieEvent = new EventEmitter<any>();
   searchCookieForm :FormGroup = new FormGroup({});
@@ -40,7 +39,7 @@ export class SearchComponent {
 
   constructor(
     private cardService: CardService,
-    private _statusBuilder: FormBuilder,
+    private formBuilder: FormBuilder,
     private filterForStatus : FilterForStatus,
     private filterForCookie : filterForCookie,
     private filterForLocalStorage : filterForLocalStorage,
@@ -54,20 +53,15 @@ export class SearchComponent {
       this.hasLocalstorageKnowledge  = result.filter(((x:KnowledgeBase)=> x.category == 'localstorage')).length >0;
     });
 
-    this.searchStatusForm = this._statusBuilder.group({
-      pending : false,
-      not_evaluate: false,
-      compliant: false,
-      not_compliant: false,
-      TBD : false
+    this.searchStatusForm = this.formBuilder.group({
+      searchStatus : []
     });
 
     this.searchStatusForm.valueChanges.subscribe((selectedValue :any) => {
-      this.searchStatus  = Object.keys(selectedValue).filter(key => selectedValue[key]) as Status[];
-      this.searchStatusEvent.emit(this.searchStatus);
+      this.searchStatusEvent.emit(selectedValue.searchStatus);
     });
 
-    this.searchCookieForm = this._statusBuilder.group({
+    this.searchCookieForm = this.formBuilder.group({
       searchDomain : "",
       searchName : ""
     });
@@ -77,7 +71,7 @@ export class SearchComponent {
     });
 
 
-    this.searchLocalStorageForm = this._statusBuilder.group({
+    this.searchLocalStorageForm = this.formBuilder.group({
       searchHost : "",
       searchKey : ""
     });
@@ -86,7 +80,7 @@ export class SearchComponent {
       this.searchLocalStorageEvent.emit(selectedValue);
     });
 
-    this.searchBeaconForm = this._statusBuilder.group({
+    this.searchBeaconForm = this.formBuilder.group({
       searchUrl : ""
     });
 
@@ -101,11 +95,7 @@ export class SearchComponent {
     switch(val){
       case 'from_values':
         this.searchStatusForm.setValue({
-          pending : false,
-          not_evaluate: false,
-          compliant: false,
-          not_compliant: false,
-          TBD : false
+          searchStatus : []
         })
         this.searchCookieForm.setValue({
           searchDomain : "",
@@ -123,12 +113,13 @@ export class SearchComponent {
   }
 
   mark_as(event: any) {
+    const test = this.searchStatusForm.value.searchStatus;
     switch (this.card?.kind) {
       case 'cookie':
         const cookieCard = this.cardService.castToCookieCard(this.card);
         this.filterForStatus.transform(
           this.filterForCookie.transform(cookieCard.cookieLines, this.searchCookieForm.value),
-          this.searchStatus)
+          this.searchStatusForm.value.searchStatus)
           .map((line :CookieLine)=> line.status = event.value);
 
         this.cardService.update(cookieCard);
@@ -137,7 +128,7 @@ export class SearchComponent {
         const localstorageCard = this.cardService.castToLocalStorageCard(this.card);
         this.filterForStatus.transform(
           this.filterForLocalStorage.transform(localstorageCard.localStorageLines, this.searchLocalStorageForm.value),
-          this.searchStatus)
+          this.searchStatusForm.value.searchStatus)
           .map((line :LocalStorageLine)=> line.status = event.value);
 
         this.cardService.update(localstorageCard);
@@ -146,11 +137,15 @@ export class SearchComponent {
         const beaconCard = this.cardService.castToBeaconCard(this.card);
         this.filterForStatus.transform(
           this.filterForBeacon.transform(beaconCard.beaconLines, this.searchBeaconForm.value),
-          this.searchStatus)
+          this.searchStatusForm.value.searchStatus)
           .map((line :BeaconLine)=> line.status = event.value);
 
         this.cardService.update(beaconCard);
         break;
     }
+  }
+
+  get allStatus(){
+    return allStatus;
   }
 }
