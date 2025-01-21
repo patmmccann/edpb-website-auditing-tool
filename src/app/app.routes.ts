e7faa6bd9279c25a355e4ee299a4a153ffc65d86
Jérôme Gorin <gorinje@gmail.com>
2025-01-21 16:19:53 +0100

    Renaming routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -7,34 +7,34 @@ export const routes: Routes = [{
 },
 {
     path: 'entries',
-    loadChildren: () => import('./modules/entries/entries-routing').then((m) => m.entriesRoutes),
+    loadChildren: () => import('./modules/entries/entries-routes').then((m) => m.entriesRoutes),
 },
 {
     path: 'knowledge_bases',
-    loadChildren: () => import('./modules/base/base-routing').then((m) => m.baseRoutes),
+    loadChildren: () => import('./modules/base/base-routes').then((m) => m.baseRoutes),
 },
 {
     path: 'template',
-    loadChildren: () => import('./modules/template/template-routing').then((m) => m.templateRoutes),
+    loadChildren: () => import('./modules/template/template-routes').then((m) => m.templateRoutes),
 },
 {
     path: 'settings',
-    loadChildren: () => import('./modules/others/settings-routing').then((m) => m.settingsRoutes),
+    loadChildren: () => import('./modules/others/settings-routes').then((m) => m.settingsRoutes),
 },
 {
     path: 'helps',
-    loadChildren: () => import('./modules/others/helps-routing').then((m) => m.helpsRoutes),
+    loadChildren: () => import('./modules/others/helps-routes').then((m) => m.helpsRoutes),
 },
 {
     path: 'browse',
-    loadChildren: () => import('./modules/browse/browse-routing').then((m) => m.browseRoutes),
+    loadChildren: () => import('./modules/browse/browse-routes').then((m) => m.browseRoutes),
 },
 {
     path: 'analysis',
-    loadChildren: () => import('./modules/analysis/analysis-routing').then((m) => m.analysisRoutes),
+    loadChildren: () => import('./modules/analysis/analysis-routes').then((m) => m.analysisRoutes),
 },
 {
     path: 'report',
-    loadChildren: () => import('./modules/report/report-routing').then((m) => m.reportRoutes),
+    loadChildren: () => import('./modules/report/report-routes').then((m) => m.reportRoutes),
 }
 ];
