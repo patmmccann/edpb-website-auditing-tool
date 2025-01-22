d82ae342fcea84abec4d867155de984d79a35f8f
Jérôme Gorin <gorinje@gmail.com>
2025-01-22 13:53:43 +0100

    Rename routing modules

--- src/app/modules/analysis/tags/tags.component.spec.ts
+++ src/app/modules/analysis/tags/tags.component.spec.ts
@@ -7,7 +7,6 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { TagsComponent } from './tags.component';
 import { MaterialAllModule } from 'src/app/material.module';
-import { AppRoutingModule } from 'src/app/app-routing.module';
 
 describe('TagsComponent', () => {
   let component: TagsComponent;
@@ -15,7 +14,7 @@ describe('TagsComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-    imports: [MaterialAllModule, AppRoutingModule, TagsComponent]
+    imports: [MaterialAllModule, TagsComponent]
 })
     .compileComponents();
 
